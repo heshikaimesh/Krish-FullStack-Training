3308e38b9c87a835f15aa0a06318266639d0d0e3
heshikaimesh <102600305+heshikaimesh@users.noreply.github.com>
2022-07-17 21:21:42 +0530

    Add files via upload

---
+++ SpringTrainingJava/src/com/krishantha/training/salesmanager/model/Employee.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.krishantha.training.salesmanager.model;
+
+/**
+ *
+ * @author Ahsika
+ */
+public class Employee {
+    
+    String employeeName;
+    String EmployeeLocation;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeLocation() {
+        return EmployeeLocation;
+    }
+
+    public void setEmployeeLocation(String EmployeeLocation) {
+        this.EmployeeLocation = EmployeeLocation;
+    }
+    
+    
+    
+}
